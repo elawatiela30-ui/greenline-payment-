1a167be5672e9f493c97f2834dd046970fcdfefa
elawatiela30-ui <elawatiela30@gmail.com>
2026-04-27 13:10:57 +0700

    Update pay.js

--- api/pay.js
+++ api/pay.js
@@ -2,6 +2,10 @@ import midtransClient from "midtrans-client";
 
 export default async function handler(req, res) {
   try {
+    if (!process.env.MIDTRANS_SERVER_KEY) {
+      return res.status(500).json({ error: "Server key tidak ada" });
+    }
+
     const snap = new midtransClient.Snap({
       isProduction: false,
       serverKey: process.env.MIDTRANS_SERVER_KEY,
@@ -14,13 +18,14 @@ export default async function handler(req, res) {
       },
     });
 
-    res.status(200).json({
+    return res.status(200).json({
       redirect_url: transaction.redirect_url,
     });
 
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
       error: error.message,
+      detail: error,
     });
   }
 }
